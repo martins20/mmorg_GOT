7b41b0fc6af13af4f35200bb6e02a2337f734699
martins20 <55673235+martins20@users.noreply.github.com>
2019-12-26 17:41:29 -0300

    updated

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -5,6 +5,14 @@ module.exports.jogo = function(application, req, res){
 		return
 	}
 
+	let comando_invalido = 'N'
+
+	if(req.query.comando_invalido == 'S') {
+		comando_invalido = 'S'
+	}
+
+	console.log(comando_invalido)
+
 	const usuario = req.session.usuario
 	const casa = req.session.casa
 
@@ -12,7 +20,7 @@ module.exports.jogo = function(application, req, res){
 	const JogoDAO = new application.app.models.JogoDAO(connection)
 
 
-	JogoDAO.iniciarJogo(res, usuario, casa)
+	JogoDAO.iniciarJogo(res, usuario, casa, comando_invalido)
 }
 
 module.exports.sair = function(application, req, res){
@@ -23,11 +31,41 @@ module.exports.sair = function(application, req, res){
 }
 
 module.exports.suditos = function(application, req, res){
+	if(req.session.autorizado !== true) {
+		res.send('Usuario Precisa fazer login')
+		return
+	}
 	
 	res.render('aldeoes')
 }
 
 module.exports.pergaminhos = function(application, req, res){
+	if(req.session.autorizado !== true) {
+		res.send('Usuario Precisa fazer login')
+		return
+	}
 	
 	res.render('pergaminhos')
+}
+
+module.exports.ordenar_acao_sudito = function(application, req, res){
+	if(req.session.autorizado !== true) {
+		res.send('Usuario Precisa fazer login')
+		return
+	}
+	
+	const dadosForm = req.body
+
+	req.assert('acao', 'Ação deve ser informada').notEmpty()
+	req.assert('quantidade', 'Quantidade deve ser informada').notEmpty()
+
+	const erros = req.validationErrors()
+
+	if(erros){
+		res.redirect('jogo?comando_invalido=S')
+		return
+	}
+
+	console.log(dadosForm)
+	res.send('tudo ok !')
 }
